a1520070cf471a606b0b4e553ff7aea37b1ddc51
Administrator <ben>
2019-08-28 12:40:41 +0800

    this关键字表示当前对象的思考题

--- java-core-tech/src/com/zmh/keywords_this/ThisDemo3.java
+++ java-core-tech/src/com/zmh/keywords_this/ThisDemo3.java
@@ -7,6 +7,9 @@ package com.zmh.keywords_this;
  */
 class Cat{
     public void print() {
+        //哪个对象调用print(),this就自动与此对象指向同一块内存地址
+        //this就是当前调用方法的对象
+        //那么之前出现的"this.属性"实际上是属于当前对象中的属性，一定是堆内存中保存的内容。
         System.out.println("this="+this);
     }
 }
@@ -15,6 +18,7 @@ public class ThisDemo3 {
         Cat cata = new Cat();
         System.out.println("cata"+cata);
         cata.print();
+        System.out.println("-----------------");
         Cat catb = new Cat();
         System.out.println("catb"+catb);
         catb.print();
---
+++ java-core-tech/src/com/zmh/keywords_this/ThisDemo4.java
@@ -0,0 +1,35 @@
+package com.zmh.keywords_this;
+
+/**
+ * @author zmh
+ * @date 2019/8/28 11:05
+ * this表示当前对象的思考题
+ */
+class A{
+    private B b;
+    public A() {//2.执行A类的构造
+        //3.为B类对象B实例化
+        this.b=new B(this);//4.this就是temp
+        this.b.get();//7.调用B类的get方法
+    }
+    public void print() {//10.调用print输出
+        System.out.println("Hello World !");
+
+    }
+}
+class B{
+    private A a;
+    public B(A a) {//5.参数a=temp
+        this.a=a;//6.保存A对象(保存temp)
+    }
+    public void get() {//8.调用此方法
+        this.a.print();//9.this.a=temp
+    }
+}
+public class ThisDemo4 {
+    public static void main(String[] args) {
+        //1.实例化A类对象，要调用A类的无参构造(由于代码
+        // 从右往左执行，执行new A()的时候temp还没有赋值，但为了表述清楚一点就假设temp已经是A类对象名了)
+        A temp=new A();
+    }
+}
